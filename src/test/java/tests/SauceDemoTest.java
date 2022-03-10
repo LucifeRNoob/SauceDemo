7ae2f416532d85b0baa502efc8c6f7a430787c02
LucifeRNoob <andrewkremenevskiy@gmail.com>
2022-03-10 15:36:45 +0300

    SauceDemo tests hw14

--- src/test/java/tests/SauceDemoTest.java
+++ src/test/java/tests/SauceDemoTest.java
@@ -1,66 +1,46 @@
 package tests;
 
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import utilities.RetryAnalyzer;
 
 public class SauceDemoTest extends BaseTest {
 
-    @Test
-    public void LogInTest() throws IOException {
+    @Test(retryAnalyzer = RetryAnalyzer.class)
+    public void LogInTest() {
         loginPage.login();
-        TakesScreenshot screenShot = ((TakesScreenshot) driver);
-        byte[] sourceFile = screenShot.getScreenshotAs(OutputType.BYTES);
-        Files.write(Paths.get("src/test/resources/screenshot1.png"), sourceFile);
         Assert.assertTrue(inventoryPage.checkInventoryPageOpened(), "User credentials are not valid");
     }
 
-    @Test
-    public void addItemToCartTest() throws IOException {
+    @Test(retryAnalyzer = RetryAnalyzer.class)
+    public void addItemToCartTest() {
         loginPage.login();
         inventoryPage.addItemToCart();
-        TakesScreenshot screenShot = ((TakesScreenshot) driver);
-        byte[] sourceFile = screenShot.getScreenshotAs(OutputType.BYTES);
-        Files.write(Paths.get("src/test/resources/screenshot2.png"), sourceFile);
         Assert.assertTrue(inventoryPage.checkRemoveButton());
     }
 
-    @Test
-    public void goToCartTest() throws IOException {
+    @Test(retryAnalyzer = RetryAnalyzer.class)
+    public void goToCartTest() {
         loginPage.login();
         inventoryPage.goToShoppingCart();
-        TakesScreenshot screenShot = ((TakesScreenshot) driver);
-        byte[] sourceFile = screenShot.getScreenshotAs(OutputType.BYTES);
-        Files.write(Paths.get("src/test/resources/screenshot3.png"), sourceFile);
         Assert.assertTrue(cartPage.checkCartIsAvailable());
     }
 
-    @Test
-    public void productPriceAndNameTest() throws IOException {
+    @Test(retryAnalyzer = RetryAnalyzer.class)
+    public void productPriceAndNameTest() {
         loginPage.login();
         inventoryPage.addItemToCart();
         inventoryPage.goToShoppingCart();
-        TakesScreenshot screenShot = ((TakesScreenshot) driver);
-        byte[] sourceFile = screenShot.getScreenshotAs(OutputType.BYTES);
-        Files.write(Paths.get("src/test/resources/screenshot4.png"), sourceFile);
         Assert.assertEquals(cartPage.getProductPrice(), "$29.99");
         Assert.assertEquals(cartPage.getProductName(), "Sauce Labs Backpack");
     }
 
-    @Test
-    public void linkedinLogoElementTest() throws IOException {
+    @Test(retryAnalyzer = RetryAnalyzer.class)
+    public void linkedinLogoElementTest() {
         loginPage.login();
         driverManager.removeTimeout();
         inventoryPage.clickLinkedinLink();
         linkedinPage.switchOnNewTab();
-        TakesScreenshot screenShot = ((TakesScreenshot) driver);
-        byte[] sourceFile = screenShot.getScreenshotAs(OutputType.BYTES);
-        Files.write(Paths.get("src/test/resources/screenshot5.png"), sourceFile);
-        Assert.assertTrue(linkedinPage.checkLogoElement(),"Element is not exist");
+        Assert.assertTrue(linkedinPage.checkLogoElement(), "Element is not exist");
     }
 }
